aaa92859a4ef740aa6be97d220652c8837346aab
yohann JOLLY <yohann.j37@gmail.com>
2025-01-22 23:33:54 +0100

    amelioration de l'api

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const headers = {
+        'Accept': 'application/json'
+    };
+
+    const carrousselMovies = [
+        "&i=tt0076759",  // Star Wars: Episode IV - A New Hope
+        "&i=tt0080684",  // Star Wars: Episode V - The Empire Strikes Back
+        "&i=tt0086190",  // Star Wars: Episode VI - Return of the Jedi
+        "&i=tt0120915",  // Star Wars: Episode I - The Phantom Menace
+        "&i=tt0121765",  // Star Wars: Episode II - Attack of the Clones
+        "&i=tt0121766",  // Star Wars: Episode III - Revenge of the Sith
+        "&i=tt2488496",  // Star Wars: Episode VII - The Force Awakens
+        "&i=tt2527336",  // Star Wars: Episode VIII - The Last Jedi
+        "&i=tt2527338"   // Star Wars: Episode IX - The Rise of Skywalker
+    ];
     const body = document.querySelector('body');
     const boutton = document.querySelector('.bouttondark');
+    const swiperWrapper = document.querySelector('.swiper-wrapper');
 
     function toggleDarkMode() {
         if (body.classList.contains('light')) {
@@ -16,42 +32,44 @@ document.addEventListener('DOMContentLoaded', () => {
 
     boutton.addEventListener('click', toggleDarkMode);
 
-    async function fetchData() {
+    async function getTendancyMovie(movie) {
         try {
-            const response = await fetch('http://www.omdbapi.com/?s=batman&apikey=3228ed4d');
-            if (!response.ok) throw new Error('Erreur de chargement des données');
+            const response = await fetch(`https://www.omdbapi.com/?apikey=929ff8b4${movie}`, {
+                method: 'GET',
+                headers: headers
+            });
 
+            if (!response.ok) throw new Error('Erreur de chargement des données');
             const data = await response.json();
 
-            if (data.Search) {
-                const swiperWrapper = document.querySelector('.swiper-wrapper');
+            if (data.Poster) {
+                const slide = document.createElement('div');
+                slide.classList.add('swiper-slide');
 
-                data.Search.forEach(movie => {
-                    const slide = document.createElement('div');
-                    slide.classList.add('swiper-slide');
+                slide.innerHTML = `
+                    <img src="${data.Poster}" alt="${data.Title}">
+                    <div>${data.Title}</div>
+                `;
 
-                    slide.innerHTML = `
-                        <img src="${movie.Pposter}" alt="${movie.Title}">
-                        <div>${movie.Title}</div>
-                    `;
-
-                    swiperWrapper.appendChild(slide);
-                });
-
-                new Swiper('.swiper', {
-                    loop: true,
-                    autoplay: {
-                        delay: 3000,
-                        disableOnInteraction: false,
-                    },
-                    speed: 1000,
-                });
+                swiperWrapper.appendChild(slide);
             }
+
         } catch (error) {
             console.error(error);
             alert('Impossible de charger les données.');
         }
     }
 
-    fetchData();
+    carrousselMovies.forEach(movie => getTendancyMovie(movie));
+
+    setTimeout(() => {
+        new Swiper('.swiper', {
+            loop: true,
+            autoplay: {
+                delay: 3000,
+                disableOnInteraction: false,
+            },
+            speed: 1000,
+        });
+    }, 1000);
 });
